689136a99fe8e570d843130e9721b52e9a91c85d
john klaras <johnklaras@johns-MacBook-Pro.local>
2017-02-26 15:32:44 -0800

    fix various responsive layout bugs, shrink soundcloud player, etc.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,25 @@ $(document).ready(function(){
   $('.slider').slick({
     autoplay: true,
     autoplaySpeed: 4000,
-    dots: true,
+    dots: false,
     fade: true,
     speed: 2000,
     cssEase: 'ease-in-out'
   });
 });
+
+// $( ".cross" ).hide();
+// $( ".menu" ).hide();
+// $( ".hamburger" ).click(function() {
+// $( ".menu" ).slideToggle( "slow", function() {
+// $( ".hamburger" ).hide();
+// $( ".cross" ).show();
+// });
+// });
+//
+// $( ".cross" ).click(function() {
+// $( ".menu" ).slideToggle( "slow", function() {
+// $( ".cross" ).hide();
+// $( ".hamburger" ).show();
+// });
+// });
